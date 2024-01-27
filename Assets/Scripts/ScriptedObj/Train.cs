dc13751cd461e6e3451645187b848e37756bac1a
julien_ssm <89165787+IjulienI@users.noreply.github.com>
2024-01-27 11:17:24 +0100

    [fix] train despawn

--- Assets/Scripts/ScriptedObj/Train.cs
+++ Assets/Scripts/ScriptedObj/Train.cs
@@ -7,8 +7,17 @@ public class Train : MonoBehaviour
 {
     [SerializeField] private float speed;
 
+    private void Start()
+    {
+        Invoke(nameof(DestroyObject), 2f);
+    }
     private void Update()
     {
         transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
+    private void DestroyObject()
+    {
+        Destroy(gameObject);
     }
 }
