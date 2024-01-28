fe038df855defa35ce03a8a19d1066b2919b6f08
Mathys Hymon <mathys.hymon@gmail.com>
2024-01-28 01:31:25 +0100

    [fix] events

--- Assets/Scripts/Player/PlayerInputHandler.cs
+++ Assets/Scripts/Player/PlayerInputHandler.cs
@@ -43,6 +43,7 @@ public class PlayerInputHandler : MonoBehaviour
         if (input.actions["shoppingList"].triggered)
         {
             PlayerListScript.Instance.DisplayList();
+            movement.OpenList();
         }
 
 
